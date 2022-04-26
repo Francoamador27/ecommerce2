d261c6f890a12793cf8769f427044dfa4e212a7c
Francoamador27 <francohugoamador@hotmail.com.ar>
2022-04-26 16:26:14 -0300

    agregado boton enviar

--- src/componentes/checkout.js
+++ src/componentes/checkout.js
@@ -1,15 +1,31 @@
-import { Link } from "react-router-dom";
+import * as React from 'react';
 import { useState, useContext } from 'react'
 import { Divider, MenuItem } from "@mui/material";
 import CartContext from "../context/Cardcontext";
 import './checkout.css';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
-
-
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 function CheckOut() {
   const { cartProducts, deletProduct, totalPrice} = useContext(CartContext)
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
    
@@ -42,7 +58,42 @@ function CheckOut() {
               <div>
               <div className="grid" >
                   <h3>Total: ${totalPrice}</h3>
-                  <button>Finalizar compra</button>
+                  <div>
+      <Button onClick={handleOpen}>Finalizar Compra</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+Finalizar Compra          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+<form>
+    <div  className='inputs'>
+    <label>Apellido</label>
+    <input ></input></div>
+   <div className='inputs'>
+    <label>Nombre</label>
+    <input ></input></div>
+    <div className='inputs'>
+    <label>Dni</label>
+    <input ></input>
+    </div>
+    <div className='inputs'>
+    <label>Email</label>
+    <input ></input>
+    </div>
+</form>
+
+              </Typography>
+              <Button variant="contained" color="success">
+        Enviar
+      </Button>
+        </Box>
+      </Modal>
+    </div>
 
 </div>
      
